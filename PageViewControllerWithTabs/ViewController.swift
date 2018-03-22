1956c8d3977c7a210a97ccf65d9b85524cfff8fe
Leela Prasad <leelaprasad@leelas-mac-mini.local>
2018-03-22 10:22:44 +0530

    Initial Commit

---
+++ PageViewControllerWithTabs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PageViewControllerWithTabs
+//
+//  Created by Leela Prasad on 22/03/18.
+//  Copyright © 2018 Leela Prasad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
